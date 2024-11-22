7934eaa14dae8691c99aba48a8c74782acddfed7
saralili5 <saralili@umich.edu>
2024-11-22 15:30:19 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,9 +1,8 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-	video = document.getElementById("player1");
-	// video = document.querySelector("#player1")
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
 	video.autoplay = false;
 	video.load();
 	// console log: both of autplay and loop are set to false
